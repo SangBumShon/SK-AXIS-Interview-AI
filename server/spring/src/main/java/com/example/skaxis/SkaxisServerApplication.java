7e66e226a028eb39fd7dea2470c9a0b56cf59f58
wochae <officialwochae@gmail.com>
2025-05-28 11:26:32 +0900

    [chore] : 스프링 서버 프로젝트명 변경

---
+++ server/spring/src/main/java/com/example/skaxis/SkaxisServerApplication.java
@@ -0,0 +1,13 @@
+
+package com.example.skaxis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkaxisServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkaxisServerApplication.class, args);
+    }
+}
