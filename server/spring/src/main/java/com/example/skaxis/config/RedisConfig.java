86780c357a74fb072d40f9a560aa67c52d719c9e
Jeonghyeon Park <jpark634@wisc.edu>
2025-05-30 15:56:25 +0900

    Redis config fixed

--- server/spring/src/main/java/com/example/skaxis/config/RedisConfig.java
+++ server/spring/src/main/java/com/example/skaxis/config/RedisConfig.java
@@ -37,17 +37,4 @@ public class RedisConfig {
         redisTemplate.setValueSerializer(serializer);
         return redisTemplate;
     }
-
-    @Bean
-    public RedisTemplate<String, String> redisPublishTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        Jackson2JsonRedisSerializer<String> serializer = new Jackson2JsonRedisSerializer<>(objectMapper, String.class);
-        redisTemplate.setValueSerializer(serializer);
-        return redisTemplate;
-    }
 }
