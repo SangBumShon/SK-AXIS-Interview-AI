b631e820fe81f73ab30f2e62ab33594e6341e4ec
SangBumShon <core5630@naver.com>
2025-06-30 11:07:52 +0900

    replace client with SK-AXIS-CLIENT main branch contents

--- client/components/PoseMiniWidget.vue
+++ client/components/PoseMiniWidget.vue
@@ -155,7 +155,7 @@ async function startRecording(personIndex) {
       formData.append('interviewee_id', state.id.toString())
       
       try {
-        const response = await fetch('http//:localhost:8000/api/v1/stt/upload', {
+        const response = await fetch('http://localhost:8000/api/v1/stt/upload', {
           method: 'POST',
           body: formData
         })
